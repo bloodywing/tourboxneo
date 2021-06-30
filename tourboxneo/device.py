bc2eb61839c7bc7ee09f95b8e4fb779e98b5df78
bloodywing <bloodywing@tastyespresso.de>
2021-06-30 22:15:02 +0200

    Better plug/unplug handling and prevent some crashes

--- tourboxneo/device.py
+++ tourboxneo/device.py
@@ -1,6 +1,13 @@
+import sys
+
 import serial
+from time import sleep
 from evdev import UInput, categorize, ecodes as e, AbsInfo
+from serial import SerialException
+import logging
 from tourboxneo.constants import MAPPING, CAP
+
+logger = logging.getLogger(__name__)
 
 
 class TourBox:
@@ -11,18 +18,33 @@ class TourBox:
         self.controller = None
 
     def start(self):
-        self.serial = serial.PosixPollSerial(self.dev_path)
+        self.serial = serial.PosixPollSerial(self.dev_path, timeout=2000)
         self.controller = UInput(CAP, name='TourBox', vendor=0x0483, product=0x5740)
 
+        reconnects = 0
         while self.serial.is_open:
-            x = self.serial.read()
+            try:
+                x = self.serial.read()
+            except SerialException:
+                logging.warning(f"Can't read: {self.dev_path}, maybe unplugged or no permission?")
+                if reconnects < 5:
+                    sleep(1)
+                    reconnects += 1
+                    continue
+                return 1
+
+            reconnects = 0
             for m in MAPPING.get(x, []):
                 self.controller.write(*m)
             self.controller.syn()
 
+
 def main():
-    t = TourBox()
-    t.start()
+    try:
+        t = TourBox(dev_path=sys.argv[1])
+        t.start()
+    except KeyboardInterrupt:
+        return
 
 
 if __name__ == '__main__':
